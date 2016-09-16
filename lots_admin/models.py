d481f60e9e65a2266302937f34492b0751b207c5
reginafcompton <reginafcompton@gmail.com>
2016-09-16 11:27:54 -0500

    Create a new view for multiple applicant check. Close #126.

--- lots_admin/models.py
+++ lots_admin/models.py
@@ -2,6 +2,7 @@ from django.db import models
 import time
 from django.conf import settings
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 class Address(models.Model):
     street = models.CharField(max_length=255)
