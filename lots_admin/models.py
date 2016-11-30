a8fdaab2b415e10e37046ef3814c42c3a8bf2233
reginafcompton <reginafcompton@gmail.com>
2016-11-30 09:09:32 -0600

    Updates to app model

--- lots_admin/models.py
+++ lots_admin/models.py
@@ -34,7 +34,7 @@ class Application(models.Model):
     organization = models.CharField(max_length=255, null=True)
     owned_pin = models.CharField(max_length=14)
     owned_address = models.ForeignKey(Address, related_name='owned_address')
-    deed_image = models.FileField(upload_to=upload_name)
+    deed_image = models.FileField(max_length=1000, upload_to=upload_name)
     contact_address = models.ForeignKey(Address, related_name='contact_address')
     phone = models.CharField(max_length=15)
     email = models.CharField(max_length=255)
