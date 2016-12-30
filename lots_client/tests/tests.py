8514d4794b668023937f2cae1623bad757711939
reginafcompton <reginafcompton@gmail.com>
2016-12-30 16:23:16 -0600

    Refactor and write one test to asert false

--- lots_client/tests/tests.py
+++ lots_client/tests/tests.py
@@ -6,17 +6,9 @@ from PIL import Image
 from lots_client.views import ApplicationForm
 
 class ApplicationFormTests(TestCase):
-    def test_application_form(self):
-        mock_image = Image.new(mode='RGB', size=(200, 200))
-        image_io = BytesIO()
-        mock_image.save(image_io, 'JPEG')
-        image_io.seek(0)
+    def make_form_data(self):
 
-        image = InMemoryUploadedFile(
-            image_io, None, 'my_deed.jpg', 'image/jpeg', 200, None
-        )
-
-        form_data = {'lot_1_address': '2255 S KOLIN AVE',
+        return {'lot_1_address': '2255 S KOLIN AVE',
             'lot_1_pin': '16-27-202-023-0000',
             'owned_address': '2257 S KOLIN AVE',
             'owned_pin': '16-27-202-024-0000',
@@ -31,9 +23,29 @@ class ApplicationFormTests(TestCase):
             'terms': True
         }
 
-        file_dict = {'deed_image': image}
+    def make_file(self):
+        mock_image = Image.new(mode='RGB', size=(200, 200))
+        image_io = BytesIO()
+        mock_image.save(image_io, 'JPEG')
+        image_io.seek(0)
 
+        image = InMemoryUploadedFile(
+            image_io, None, 'my_deed.jpg', 'image/jpeg', 200, None
+        )
+
+        return {'deed_image': image}
+
+    def test_valid_application_form(self):
+        form_data = self.make_form_data()
+        file_dict = self.make_file()
         form = ApplicationForm(data=form_data, files=file_dict)
         self.assertTrue(form.is_valid())
+
+    def test_missing_lot_address(self):
+        form_data = self.make_form_data()
+        file_dict = self.make_file()
+        form_data['lot_1_address'] = None
+        form = ApplicationForm(data=form_data, files=file_dict)
+        self.assertFalse(form.is_valid())
 
 
