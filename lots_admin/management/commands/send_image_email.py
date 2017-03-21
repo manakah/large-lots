edde450ec6f373b03f0ca5a5e17c72311ee1cffc
reginafcompton <reginafcompton@gmail.com>
2017-03-21 16:16:56 -0500

    Refine email management command and add/edit templates

--- lots_admin/management/commands/send_image_email.py
+++ lots_admin/management/commands/send_image_email.py
@@ -3,31 +3,39 @@ from django.core.mail import EmailMultiAlternatives
 from django.template.loader import get_template
 from django.conf import settings
 
-from lots_admin.models import Application
+from lots_admin.models import Application, ApplicationStatus
 
 class Command(BaseCommand):
-    help = 'Remove potentially malicious things from uploaded files'
+    help = 'Send emails to applicants who need to resubmit deeds'
 
     def handle(self, *args, **options):
         applications = Application.objects.all()
 
         for application in applications:
-            if application.deed_image == '':
+            # Check for ward 27.
+            application_status_objs = ApplicationStatus.objects.filter(application_id=application.id)
+            wards = []
+            for status in application_status_objs:
+                wards.append(status.lot.address.ward)
+
+            # if application.deed_image == '' and '27' not in wards:
+            if application.deed_image == '' and '3' in wards:
                 self.send_email(application)
-    
+
+
     def send_email(self, application):
         context = {
             'app': application,
             'lots': application.lot_set.all()
         }
-        
-        html_template = get_template('deed_upload_email.html')
-        txt_template = get_template('deed_upload_email.txt')
+
+        html_template = get_template('deed_inquiry_email.html')
+        txt_template = get_template('deed_inquiry_email.txt')
 
         html_content = html_template.render(context)
         txt_content = txt_template.render(context)
-        
-        msg = EmailAlternatives('Special notice from Large Lots',
+
+        msg = EmailMultiAlternatives('Special notice from Large Lots',
                                 txt_content,
                                 settings.EMAIL_HOST_USER,
                                 [application.email])
