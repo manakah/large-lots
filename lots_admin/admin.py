f99d03fb1cc90533b658d62a88aaaf07d18bb1a1
reginafcompton <reginafcompton@gmail.com>
2017-03-23 15:00:43 -0500

    Improve django-admin

--- lots_admin/admin.py
+++ lots_admin/admin.py
@@ -2,10 +2,16 @@ from django.contrib import admin
 
 from lots_admin.models import Address, Application, Lot, ApplicationStep, Review, DenialReason, ApplicationStatus
 
+class AdminApplication(admin.ModelAdmin):
+    search_fields = ['first_name', 'last_name', 'id']
+
+class AdminApplicationStatus(admin.ModelAdmin):
+    search_fields = ['application__first_name', 'application__last_name', 'application__id']
+
 admin.site.register(Address)
-admin.site.register(Application)
+admin.site.register(Application, AdminApplication)
 admin.site.register(Lot)
 admin.site.register(ApplicationStep)
 admin.site.register(Review)
 admin.site.register(DenialReason)
-admin.site.register(ApplicationStatus)
+admin.site.register(ApplicationStatus, AdminApplicationStatus)
