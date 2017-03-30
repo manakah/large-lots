c994184f35bd61062cddebacdf0204a520ad4e75
reginafcompton <reginafcompton@gmail.com>
2017-03-30 09:59:40 -0500

    Email ready for Sheeanna

--- lots_admin/management/commands/send_image_email.py
+++ lots_admin/management/commands/send_image_email.py
@@ -27,7 +27,8 @@ class Command(BaseCommand):
             except:
                 second_ward = ''
 
-            if application.deed_image == '' and '16' in wards and first_ward not in sent_emails and second_ward not in sent_emails:
+            # if application.deed_image == '' and '16' in wards and first_ward not in sent_emails and second_ward not in sent_emails:
+            if application.id == '2510':
                 print(application.first_name, application.last_name, " - Application ID", application.id)
                 print('Wards:', wards)
                 try:
