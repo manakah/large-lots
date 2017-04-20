ddce16302328bfde18a3ce2c626073a3f2398e24
reginafcompton <reginafcompton@gmail.com>
2017-04-20 11:23:15 -0500

    More emails

--- lots_admin/management/commands/send_image_email.py
+++ lots_admin/management/commands/send_image_email.py
@@ -13,13 +13,13 @@ class Command(BaseCommand):
 
         print("Emails sent to:")
         for application in applications:
-            # Check for ward 27.
             application_status_objs = ApplicationStatus.objects.filter(application_id=application.id)
             wards = []
             for status in application_status_objs:
                 wards.append(status.lot.address.ward)
 
             # if application.deed_image == '' and '27' not in wards:
+            # Ward numbers.
             sent_emails = ['3', '4', '5', '6', '7', '8', '9', '20', '34', '10', '11', '15', '16', '17', '18', '19', '21', '22', '24', '26', '28', '29', '37']
 
             try:
@@ -33,11 +33,9 @@ class Command(BaseCommand):
                 second_ward = ''
 
             # if application.deed_image == '' and '37' in wards and first_ward not in sent_emails and second_ward not in sent_emails:
-            blank_deeds = [329, 2602, 1693, 1788, 1530, 1434, 672, 206, 1140, 2200, 2110, 2376, 444, 2310, 1696, 2048, 2045, 2047, 2049, 2052, 1402, 1651, 163, 2599, 672, 2048, 2147, 1651, 206, 1140, 1001, 1004, 2200, 2110, 2310, 444, 1693, 2539, 2554, 329, 2604, 1530, 1434, 1512, 2113, 281, 1522, 2673, 2599, 2104, 622, 1473, 978, 984, 2579, 1788, 1696, 2045, 2052, 1954, 2658, 2016, 2607, 816, 1405, 53, 163, 2116, 2117, 150]
+            blank_deeds = [1330, 1332, 1205, 2015, 2696, 1395, 575, 2723, 1473, 168, 562, 331, 2170, 1607, 301, 2180, 1128, 2422, 19, 2565, 578, 1405, 676, 1725, 497]
 
-            # [135, 139, 146, 153]
-
-            if application.deed_image == '' and application.id == 2416:
+            if application.deed_image == '' and application.id in blank_deeds:
                 print(application.first_name, application.last_name, " - Application ID", application.id)
                 print('Wards:', wards)
                 try:
