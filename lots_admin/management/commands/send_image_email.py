9fd806a52d6f458f7386ad764f9a683896e00b1e
reginafcompton <reginafcompton@gmail.com>
2017-06-07 11:55:52 -0500

    New denial email

--- lots_admin/management/commands/send_image_email.py
+++ lots_admin/management/commands/send_image_email.py
@@ -158,6 +158,60 @@ class Command(BaseCommand):
 
                             time.sleep(5)
 
+        if options['blank_deed_denial']:
+            application_statuses = ApplicationStatus.objects.all()
+            send_email = True
+            print("Emails sent to:")
+            for app in application_statuses:
+                if app.current_step:
+                    if app.current_step.step == 2 and app.denied == False and app.application.deed_image == '':
+
+                        # Check if the same application is on Step 4 or 5
+                        for app_duplicate in application_statuses:
+                            if (app.application.first_name == app_duplicate.first_name) and (app.application.last_name == app_duplicate.last_name) and (app.lot == app_duplicate.lot) and (app.application.id != app_duplicate.application.id):
+                                print("Duplicate Found:")
+                                print(app.application.first_name, app.application.last_name, " - Application ID", app.application.id, " - Status", app.id)
+                                send_email = False
+
+                        # if send_email == True
+                        #     print(app.application.first_name, app.application.last_name, " - Application ID", app.application.id, " - Status", app.id)
+                        #     print(datetime.now())
+
+                        #     try:
+                        #         self.send_denial_email(app)
+                        #     except SMTPException as smtp_e:
+                        #         print(smtp_e)
+                        #         print("Not able to send email due to smtp exception.")
+                        #     except Exception as e:
+                        #         print(e)
+                        #         print("Not able to send email.")
+
+                        #     time.sleep(5)
+
+    def send_denial_email(self, application_status):
+        reason, created = DenialReason.objects.get_or_create(value=DENIAL_REASONS['document'])
+        review = Review(reviewer='Jeanne Chandler', email_sent=True, denial_reason=reason, application=application_status, step_completed=2)
+        review.save()
+
+        context = {
+            'app': application_status.application,
+            'lot': application_status.lot,
+            'review': # create a review...
+        }
+
+        html_template = get_template('denial_html_email.html')
+        txt_template = get_template('denial_text_email.txt')
+
+        html_content = html_template.render(context)
+        txt_content = txt_template.render(context)
+
+        msg = EmailMultiAlternatives('Large Lots Application',
+                                txt_content,
+                                settings.EMAIL_HOST_USER,
+                                [application_status.application.email])
+
+        msg.attach_alternative(html_content, 'text/html')
+        msg.send()
 
     def send_deed_email(self, application):
         context = {
