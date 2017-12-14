db16aefdfdf9cef83daf5d69c25517f1debf4095
reginafcompton <reginafcompton@gmail.com>
2017-12-14 09:03:57 -0600

    Remove log comments and revise toggle JS

--- lots/static/js/largelots_mapmaker.js
+++ lots/static/js/largelots_mapmaker.js
@@ -173,7 +173,7 @@ var LargeLots = {
           soldSQL = "(select cartodb_id, the_geom, the_geom_webmercator, pin, pin_nbr, street_name, street_direction, street_type, ward::int, community from all_sold_lots) UNION ALL (select cartodb_id, the_geom, the_geom_webmercator, pin, pin_nbr, street_name, street_direction, street_type, ward::int, community from large_lots_2016_fall_expansion where pin_nbr in (" + pins_sold + "))";
       }
       else {
-          soldSQL = '(select * from all_sold_lots where false)';
+          soldSQL = 'select * from all_sold_lots where false';
       }
       if(checks.indexOf('current') >= 0){
           currentSQL = 'select * from ' + LargeLots.cartodb_table + LargeLots.mainWhere;
